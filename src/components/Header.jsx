1ed4591e855ec28e739dcf04469755e222060169
farhanlibra1984 <farhanlibra1984@outlook.com>
2024-05-21 08:21:11 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,9 @@ import style from '../components/Header.module.css'
 const Header = () => {
   return (
     
-    <div>
+    <div className={style.nav}>
         <nav className="navbar navbar-expand-lg" style={{backgroundColor:"blue"}}>
-  <div className={style.nav}>
+  <div >
    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
